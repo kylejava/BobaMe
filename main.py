65f3ea114a42df3bb85693ec033ee1b683aada78
kyle java <42562960+kylejava@users.noreply.github.com>
2019-05-03 18:06:16 -0700

    small changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import json
 
 def search(city , offset):
-    PARAMETERS = {'term':'boba',
+    PARAMETERS = {'term': 'boba',
                     'limit': 3,
                     'radius': 10000,
                     'location': city,
@@ -21,7 +21,7 @@ def search(city , offset):
 
 #API stuff
 buisness_id = 'aAMbdEgSzj7k5UmGQu9fYg'
-API_KEY = YOUR API KEY
+API_KEY = 'KyOYfJZS9HfNvVPmVD-QFj4ITXBV_ZAKLLjI2TxQHrlsGnfo0mVJJB8xtyV5BlW_p5-S_RmwhF8nrenZer_GC5lz6r9gZCM2pyi1rurotBNSebPFqOBQh_jvl4bLXHYx'
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 HEADERS = {'Authorization': 'Bearer %s' % API_KEY}
 
@@ -29,23 +29,23 @@ HEADERS = {'Authorization': 'Bearer %s' % API_KEY}
 #Main Function\
 def main():
     print("Welcome to BobaMe!")
-    print()
-    user_city = input("Enter your city or zip code to find the nearest boba shop: ")
+    print("")
+    user_city = raw_input("Enter your city or zip code to find the nearest boba shop: ")
     user_offset = 0
 
     search(user_city , user_offset)
-    print()
-    try_again = input("Would you like to find more options? y/n ")
+    print("")
+    try_again = raw_input("Would you like to find more options? y/n ")
 
     while(try_again == "y" or try_again == "Y"):
-        while(user_offset != 9):
+        while(user_offset != 6):
             user_offset = user_offset + 3
             search(user_city , user_offset)
-            print()
-            if(user_offset == 9):
+            print("")
+            if(user_offset == 6):
                 break
             else:
-                try_again = input("Would you like to find more options? y/n ")
+                try_again = raw_input("Would you like to find more options? y/n ")
         break
 
 
