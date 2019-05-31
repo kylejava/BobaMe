c3b6d90c0e6aa61dc1f8bdd06054f166ff0bd66b
JVAVmusic <kyleedwardgjava@gmail.com>
2019-05-31 13:01:48 -0700

    lots of commits

---
+++ get_a.py
@@ -0,0 +1,2 @@
+def getAPI():
+    return 'KyOYfJZS9HfNvVPmVD-QFj4ITXBV_ZAKLLjI2TxQHrlsGnfo0mVJJB8xtyV5BlW_p5-S_RmwhF8nrenZer_GC5lz6r9gZCM2pyi1rurotBNSebPFqOBQh_jvl4bLXHYx'
--- main.py
+++ main.py
@@ -14,17 +14,21 @@ def search(city , offset):
     response = requests.get(url = ENDPOINT , params = PARAMETERS, headers = HEADERS)
 
     buisness_data = response.json()
+    #print(buisness_data[])
 
     for biz in buisness_data['businesses']:
-        print(biz['name'])
-        print((str(biz['location']['address1'])) +(" ")+ (str(biz['location']['city']))+ (" ")+ (str(biz['location']['state']))+ (" ") + (str(biz['location']['zip_code'])))
-        print(biz['phone'])
-        print((str(biz['rating'])) + (" Stars"))
-        print()
-
+        if(KeyError == 'buisnesses'):
+            return None
+        else:
+            shop_name = (biz['name'])
+            shop_location = ((str(biz['location']['address1'])) +(" ")+"\n"+ (str(biz['location']['city']))+ (" ") + (str(biz['location']['state']))+ (" ") + (str(biz['location']['zip_code'])))
+            phone = (biz['phone'])
+            review = ((str(biz['rating'])) + (" Stars"))
+            image = (biz['image_url'])
+            return [shop_name , shop_location , phone , review , image]
 #API stuff
 buisness_id = 'aAMbdEgSzj7k5UmGQu9fYg'
-API_KEY = 'YOUR API KEY'
+API_KEY = 'KyOYfJZS9HfNvVPmVD-QFj4ITXBV_ZAKLLjI2TxQHrlsGnfo0mVJJB8xtyV5BlW_p5-S_RmwhF8nrenZer_GC5lz6r9gZCM2pyi1rurotBNSebPFqOBQh_jvl4bLXHYx'
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 HEADERS = {'Authorization': 'Bearer %s' % API_KEY}
 
@@ -34,22 +38,29 @@ def main():
     print("Welcome to BobaMe!")
     print("")
 
-    user_city = input("Enter your city or zip code to find the nearest boba shop: ")
+    res = requests.get('https://ipinfo.io/')
+    data = res.json()
+
+    user_city = data['loc']
     user_offset = 0
 
-    search(user_city , user_offset)
+    x = search(user_city , user_offset)
+
+    print(x)
     print("")
     try_again = input("Would you like to find more options? y/n ")
-
     while(try_again == "y" or try_again == "Y"):
         while(user_offset != 6):
             user_offset = user_offset + 3
-            search(user_city , user_offset)
+            x = search(user_city , user_offset)
+            print(x)
             print("")
             if(user_offset == 6):
                 break
             else:
                 try_again = input("Would you like to find more options? y/n ")
+                if(try_again == "n"):
+                    break
         break
 
 
