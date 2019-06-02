bd6ba9a169602f216e21d5ae0896b6f7e617f963
kyle java <42562960+kylejava@users.noreply.github.com>
2019-06-01 22:58:17 -0700

    mac

--- func_app.py
+++ func_app.py
@@ -1,40 +1,82 @@
 import requests
 import json
 from get_a import getAPI
-class function():
+#class function():
+def search(city , offset):
     buisness_id = 'aAMbdEgSzj7k5UmGQu9fYg'
     API_KEY = getAPI()
     ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
     HEADERS = {'Authorization': 'Bearer %s' % API_KEY}
+    PARAMETERS = {'term': 'boba',
+                    'limit': 1,
+                    'radius': 10000,
+                    'location': city,
+                    'offset': offset
+                    }
+    response = requests.get(url = ENDPOINT , params = PARAMETERS, headers = HEADERS)
+    buisness_data = response.json()
 
-    def search(city , offset):
-        PARAMETERS = {'term': 'boba',
-                        'limit': 1,
-                        'radius': 10000,
-                        'location': city,
-                        'offset': offset
-                        }
+    for biz in buisness_data['businesses']:
+        shop_name = (biz['name'])
+        shop_location = ((str(biz['location']['address1'])) +(" ")+"\n"+ (str(biz['location']['city']))+ (" ") + (str(biz['location']['state']))+ (" ") + (str(biz['location']['zip_code'])))
+        phone = (biz['phone'])
+        review = ((str(biz['rating'])) + (" Stars"))
+        image = (biz['image_url'])
+        return [shop_name , shop_location , phone , review , image]
 
-        response = requests.get(url = function.ENDPOINT , params = PARAMETERS, headers = function.HEADERS)
+    #@staticmethod
+def locate():
+    #res = requests.get('https://ipinfo.io/' , verify = False )
+    #data = res.json()
+    #user_city = data['postal']
+    user_city = "Murrieta"
+    offset = 0
+    self.offset = 0
+    shop = function.search(user_city , offset)
+    self.shop = shop
+    shop_name = shop[0]
+    shop_location = shop[1]
+    shop_phone = shop[2]
+    shop_review = shop[3]
+    shop_image = shop[4]
+    self.bobashop.text = shop_name
+    self.bobalocation.text = shop_location
+    self.bobaphone.text = shop_phone
+    self.bobareview.text = shop_review
+    print(self.offset)
+    self.bobaimage = shop_image
 
-        buisness_data = response.json()
 
-        for biz in buisness_data['businesses']:
-            shop_name = (biz['name'])
-            shop_location = ((str(biz['location']['address1'])) +(" ")+"\n"+ (str(biz['location']['city']))+ (" ") + (str(biz['location']['state']))+ (" ") + (str(biz['location']['zip_code'])))
-            phone = (biz['phone'])
-            review = ((str(biz['rating'])) + (" Stars"))
-            image = (biz['image_url'])
-            return [shop_name , shop_location , phone , review , image]
-
-    def locate(self):
+def search_again(self):
+    if(self.offset < 7):
         res = requests.get('https://ipinfo.io/')
         data = res.json()
         user_city = data['postal']
-        offset = 0
-        self.offset = 0
-        shop = function.search(user_city , offset)
-        self.shop = shop
+        self.offset += 1
+        self.shop = function.search(user_city , (self.offset))
+        shop = self.shop
+        shop_name = shop[0]
+        shop_location = shop[1]
+        shop_phone = shop[2]
+        shop_review = shop[3]
+        shop_image = shop[4]
+        self.bobashop.text = shop_name
+        self.bobalocation.text = shop_location
+        self.bobaphone.text = shop_phone
+        self.bobareview.text = shop_review
+        self.bobaimage = shop_image
+        print(self.offset)
+    elif(self.offset >= 7):
+        print(self.offset)
+
+def go_back(self):
+    if(self.offset > 0):
+        res = requests.get('https://ipinfo.io/')
+        data = res.json()
+        user_city = data['postal']
+        self.offset = self.offset - 1
+        self.shop = function.search(user_city , (self.offset))
+        shop = self.shop
         shop_name = shop[0]
         shop_location = shop[1]
         shop_phone = shop[2]
@@ -46,48 +88,5 @@ class function():
         self.bobareview.text = shop_review
         print(self.offset)
         self.bobaimage = shop_image
-
-
-    def search_again(self):
-        if(self.offset < 7):
-            res = requests.get('https://ipinfo.io/')
-            data = res.json()
-            user_city = data['postal']
-            self.offset += 1
-            self.shop = function.search(user_city , (self.offset))
-            shop = self.shop
-            shop_name = shop[0]
-            shop_location = shop[1]
-            shop_phone = shop[2]
-            shop_review = shop[3]
-            shop_image = shop[4]
-            self.bobashop.text = shop_name
-            self.bobalocation.text = shop_location
-            self.bobaphone.text = shop_phone
-            self.bobareview.text = shop_review
-            self.bobaimage = shop_image
-            print(self.offset)
-        elif(self.offset >= 7):
-            print(self.offset)
-
-    def go_back(self):
-        if(self.offset > 0):
-            res = requests.get('https://ipinfo.io/')
-            data = res.json()
-            user_city = data['postal']
-            self.offset = self.offset - 1
-            self.shop = function.search(user_city , (self.offset))
-            shop = self.shop
-            shop_name = shop[0]
-            shop_location = shop[1]
-            shop_phone = shop[2]
-            shop_review = shop[3]
-            shop_image = shop[4]
-            self.bobashop.text = shop_name
-            self.bobalocation.text = shop_location
-            self.bobaphone.text = shop_phone
-            self.bobareview.text = shop_review
-            print(self.offset)
-            self.bobaimage = shop_image
-        elif(self.offset == 0):
-            print(self.offset)
+    elif(self.offset == 0):
+        print(self.offset)
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import kivy
 import requests
+import sys
+from pprint import pprint
 import json
 import random
-import urllib.request
 from get_a import getAPI
 import func_app
-from func_app import function
+import urllib3
+from time import sleep
+import codecs
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.lang import Builder
@@ -16,7 +19,29 @@ from kivy.uix.screenmanager import ScreenManager , Screen
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty
 from kivy.uix.textinput import TextInput
+urllib3.disable_warnings()
 
+def search(city , offset):
+    buisness_id = 'aAMbdEgSzj7k5UmGQu9fYg'
+    API_KEY = getAPI()
+    ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+    HEADERS = {'Authorization': 'Bearer %s' % API_KEY}
+    PARAMETERS = {'term': 'boba',
+                    'limit': 1,
+                    'radius': 10000,
+                    'location': city,
+                    'offset': offset
+                    }
+    response = requests.get(url = ENDPOINT , params = PARAMETERS, headers = HEADERS)
+    buisness_data = response.json()
+
+    for biz in buisness_data['businesses']:
+        shop_name = (biz['name'])
+        shop_location = ((str(biz['location']['address1'])) +(" ")+"\n"+ (str(biz['location']['city']))+ (" ") + (str(biz['location']['state']))+ (" ") + (str(biz['location']['zip_code'])))
+        phone = (biz['phone'])
+        review = ((str(biz['rating'])) + (" Stars"))
+        image = (biz['image_url'])
+        return [shop_name , shop_location , phone , review , image]
 
 
 
@@ -30,19 +55,92 @@ class MainScreen(Screen):
     bobareview = ObjectProperty(0)
     bobaimage = ObjectProperty(" ")
     current = ""
-    res = requests.get('https://ipinfo.io/')
-    data = res.json()
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        self.user_city = ""
     def on_enter(self):
-        function.locate(self)
+        #url = 'https://ipinfo.io/'
+        #res = requests.get(url, auth=('user' , 'pass') , verify=False)
+        #data = res.json()
+        send_url = "http://api.ipstack.com/check?access_key=f787f7960a63899a81ebae839b4f4903"
+        geo_req = requests.get(send_url)
+        geo_json = json.loads(geo_req.text)
+        latitude = geo_json['latitude']
+        longitude = geo_json['longitude']
+        user_city = geo_json['zip']
+
+        #user_city = data['postal']
+        offset = 0
+        self.offset = 0
+        shop = search(user_city , offset)
+        self.shop = shop
+        shop_name = shop[0]
+        shop_location = shop[1]
+        shop_phone = shop[2]
+        shop_review = shop[3]
+        shop_image = shop[4]
+        self.bobashop.text = shop_name
+        self.bobalocation.text = shop_location
+        self.bobaphone.text = shop_phone
+        self.bobareview.text = shop_review
+        print(self.offset)
+        self.bobaimage = shop_image
 
     def addSearch(self):
-        function.search_again(self)
+        if(self.offset < 7):
+            #res = requests.get('https://ipinfo.io/')
+            #data = res.json()
+            #user_city = data['postal']
+            send_url = "http://api.ipstack.com/check?access_key=f787f7960a63899a81ebae839b4f4903"
+            geo_req = requests.get(send_url)
+            geo_json = json.loads(geo_req.text)
+            latitude = geo_json['latitude']
+            longitude = geo_json['longitude']
+            user_city = geo_json['zip']
+
+            self.offset += 1
+            self.shop = search(user_city , (self.offset))
+            shop = self.shop
+            shop_name = shop[0]
+            shop_location = shop[1]
+            shop_phone = shop[2]
+            shop_review = shop[3]
+            shop_image = shop[4]
+            self.bobashop.text = shop_name
+            self.bobalocation.text = shop_location
+            self.bobaphone.text = shop_phone
+            self.bobareview.text = shop_review
+            self.bobaimage = shop_image
+            print(self.offset)
+        elif(self.offset >= 7):
+            print(self.offset)
+
 
     def subSearch(self):
-        function.go_back(self)
+        if(self.offset > 0):
+            #res = requests.get('https://ipinfo.io/')
+            #data = res.json()
+            #user_city = data['postal']
+            send_url = "http://api.ipstack.com/check?access_key=f787f7960a63899a81ebae839b4f4903"
+            geo_req = requests.get(send_url)
+            geo_json = json.loads(geo_req.text)
+            latitude = geo_json['latitude']
+            longitude = geo_json['longitude']
+            user_city = geo_json['zip']
+            self.offset = self.offset - 1
+            self.shop = search(user_city , (self.offset))
+            shop = self.shop
+            shop_name = shop[0]
+            shop_location = shop[1]
+            shop_phone = shop[2]
+            shop_review = shop[3]
+            shop_image = shop[4]
+            self.bobashop.text = shop_name
+            self.bobalocation.text = shop_location
+            self.bobaphone.text = shop_phone
+            self.bobareview.text = shop_review
+            print(self.offset)
+            self.bobaimage = shop_image
+        elif(self.offset == 0):
+            print(self.offset)
+
 
 
 
@@ -51,9 +149,10 @@ class WindowManager(ScreenManager):
 
 
 kv = Builder.load_file("bobame.kv")
-
+sm = WindowManager()
 class BobaMeApp(App):
     def build(self):
         return kv
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     BobaMeApp().run()
